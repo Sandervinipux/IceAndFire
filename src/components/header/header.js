597e8bf1f387a4a2fca8d29082bb71b47a16f26f
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-10-14 11:16:37 +0300

    pages

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h3 classname="header-title">
+                <Link to='/'>
+                Game of Thrones DB
+                </Link>
+            </h3>
+            <ul classname="header-list">
+                <li>
+                    <Link to='/characters/'>Characters</Link>
+                </li>
+                <li>
+                    <Link to='/houses/'>Houses</Link>
+                </li>
+                <li>
+                    <Link to='/books/'>Books</Link>
+                </li>
+            </ul>
+        </div>
+    );   
+};
+export default Header;
