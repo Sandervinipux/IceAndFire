a67de585ffe03eab371541a9fd1cadaea8e79dd2
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-10-18 11:05:45 +0300

    gotovo

---
+++ src/components/pages/index.js
@@ -0,0 +1,10 @@
+import CharacterPage from "./CharacterPage";
+import BooksPage from './booksPage';
+import HousesPage from './housesPage';
+import BooksItem from './booksItem';
+export { 
+    CharacterPage,
+    BooksPage,
+    HousesPage,
+    BooksItem
+}
